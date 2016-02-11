331fb2a836d731bed1033221e0177bd9f9424107
R7038 <ITboud00@gmail.com>
2016-02-11 11:32:01 -0500

    Create form1.cs

---
+++ form1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox51_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox52_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox53_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox54_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox55_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox56_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox58_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
