e4ad18f3111abcadd7764168f24494e180430905
intenseFunkid <intensefunkid@gmail.com>
2016-03-31 10:32:56 -0400

    Added files via upload
    Test upload of entire project from school shared drive

---
+++ projectOverlord Prototype/randomTableList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectOverlord
+{
+
+    struct randomTable {
+        int tableID;
+        string title;
+        LinkedList<string> table;
+    }
+
+    class randomTableList {
+
+    }
+}
