ce61343a7d7aada4d6a8a0f68f2dcca1838015f4
BKing7 <BKing7@users.noreply.github.com>
2016-03-31 11:46:53 -0400

    Added files via upload

---
+++ Project Overlord v2/Project-Overlord-Prototype/projectOverlord Prototype/charStatList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectOverlord
+{
+    //Pathfinder statistics payload struct
+    struct statBlockPF {
+        int blockID;
+        string name;
+        int STR, CON, DEX, INT, WIS, CHA;
+        
+
+    }
+
+    class charStatList {
+        LinkedList<statBlockPF> charList;
+
+        
+    }
+}
