b8d4b671996e608f0442c061aec336aa6e406f07
BKing7 <BKing7@users.noreply.github.com>
2016-05-01 15:36:00 -0400

    random tables page working
    All random table page features are now working.
    
    Still need to make sure that the data structure outputs all of it's data to the form for character and calendar pages when an xml file is loaded. (It deSerializes from the file into the data structure, but no idea how/if those pages are triggering a load to the form). Ditto for saving to the data structure from the form for calendar and character pages.

---
+++ Brandon v 0.2.03/gameDateList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectOverlord {
+
+    //Payload struct
+    public struct gameDateEntry {
+        public int gameDateID;      //Numeric identifier 
+        public string entry;        //Notes for game day
+
+        //Payload constructor
+        public gameDateEntry(int id, string ent) {
+            gameDateID = id;
+            entry = ent;
+        }
+    }
+
+    class gameDateList {
+        private LinkedList<gameDateEntry> gDateList = new LinkedList<gameDateEntry>();
+        //private LinkedList<gameDateEntry> index;
+        private gameDateEntry error = new gameDateEntry(-1, "<!>ERROR");
+
+        public void clearList() {
+            gDateList.Clear();
+        }
+
+        //Get first payload in list
+        public gameDateEntry getFirst() {
+
+            if (gDateList.Count != 0) {
+                return gDateList.First.Value;
+            } else {
+                return error;
+            }
+
+        }
+
+        //Get payload stored next after specified gameDateID
+        public gameDateEntry getNext(int targetID) {
+            LinkedListNode<gameDateEntry> current = gDateList.First;
+
+            while (current != null) {
+
+                if (current.Value.gameDateID == targetID) {
+                    if (current.Next != null) {
+                        return current.Next.Value;
+                    } else {
+                        break;
+                    }
+                }
+
+                current = current.Next;
+            }
+            return error;
+        }
+
+        //Get payload stored next before specified gameDateID
+        public gameDateEntry getPrev(int targetID) {
+            LinkedListNode<gameDateEntry> current = gDateList.First;
+
+            while (current != null) {
+
+                if (current.Value.gameDateID == targetID) {
+                    if (current.Previous != null) {
+                        return current.Previous.Value;
+                    } else {
+                        break;
+                    }
+                }
+
+                current = current.Next;
+            }
+            return error;
+        }
+
+        //Get last payload in list
+        public gameDateEntry getLast() {
+
+            if (gDateList.Count != 0) {
+                return gDateList.Last.Value;
+            } else {
+                return error;
+            }
+
+        }
+
+        //Add specified payload to list
+        public Boolean addEntry(gameDateEntry newDate) {
+
+            if (gDateList.Count == 0) {
+                gDateList.AddFirst(newDate);
+                return true;
+            }
+
+            LinkedListNode<gameDateEntry> current = gDateList.First;
+
+            while (current != null) {
+
+                if (current.Value.gameDateID == newDate.gameDateID) {           //If updating an entry
+                    current.Value = newDate;
+                    return true;                
+                } else if (current.Value.gameDateID > newDate.gameDateID) {     //Inserting within list
+                    gDateList.AddBefore(current, newDate);
+                    return true;
+                } else if (current == gDateList.Last) {                         //Inserting at end of list
+                    gDateList.AddAfter(current, newDate);
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        //Remove payload with specified ID from list
+        public Boolean removeEntry(int targetID) {
+            LinkedListNode<gameDateEntry> current = gDateList.First;
+
+            while (current != null) {
+
+                if (current.Value.gameDateID == targetID) {
+                    gDateList.Remove(current);
+                    return true;
+                }
+
+                current = current.Next;
+            }
+            
+            return false;
+        }
+
+        //Retrieve payload with specified 
+        public gameDateEntry retrieveEntry(int targetID) {
+            LinkedListNode<gameDateEntry> current = gDateList.First;
+
+            while (current != null) {
+
+                if (current.Value.gameDateID == targetID) {
+                    return current.Value;
+                }
+
+                current = current.Next;
+            }
+            
+            return error;
+        }
+
+    }
+}
