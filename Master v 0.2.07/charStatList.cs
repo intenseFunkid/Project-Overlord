a5b9f324aea01bc1370c93878df16cdc20fb8f13
BKing7 <BKing7@users.noreply.github.com>
2016-05-02 23:48:06 -0400

    All PagesSet! No obvious errors during testing
    Added the checkboxes on the character page to the data structure and set them to save, load, and clear like every other field.

---
+++ Master v 0.2.07/charStatList.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectOverlord
+{
+    //Pathfinder statistics payload struct
+    public class statBlockPF
+    {
+        public int blockID;
+        public string playerName;
+        public string name = "New Character";
+        public string raceClass = "";
+        public int STR, CON, DEX, INT, WIS, CHA;
+        public int AC, touchAC, flatAC, fort, reflex, will, speed, reach, initiative, BAB, CMB, CMD;
+        public int sklAcro, sklAppr, sklBluf, sklClim,
+                   sklCraf, sklDipl, sklDisa, sklDisg,
+                   sklEsca, sklFly, sklHand, sklHeal,
+                   sklInti, sklLing, sklPerc, sklPerf,
+                   sklProf, sklRide, sklSens, sklSlei,
+                   sklSpel, sklStel, sklSurv, sklSwim,
+                   sklUseD;
+        public int knwArca, knwDung, knwEngi, knwGeog,
+                   knwHist, knwLoca, knwNatu, knwNobi,
+                   knwPlan, knwReli;
+        //Trained Skill or not
+        public bool     acroTrn, apprTrn, blufTrn, climTrn,
+                        crafTrn, diplTrn, disaTrn, disgTrn,
+                        escaTrn, flyTrn, handTrn, healTrn,
+                        intiTrn, lingTrn, percTrn, perfTrn,
+                        profTrn, rideTrn, sensTrn, sleiTrn,
+                        spelTrn, steaTrn, survTrn, swimTrn,
+                        useMTrn;
+        public bool     arcaTrn, dungTrn, engiTrn, geogTrn,
+                        histTrn, locaTrn, natuTrn, nobiTrn,
+                        planTrn, reliTrn;
+        public List<string> classFeat = new List<string>();
+        public List<string> equipment = new List<string>();
+        public List<string> spells = new List<string>();
+
+        public statBlockPF()
+        {
+            blockID = -1; // <------ Is this really needed?
+            playerName = "Unknown Player";
+            name = "New Character";
+            raceClass = "Unknown Race & Class";
+            STR = CON = DEX = INT = WIS = CHA = 10;
+            AC = touchAC = flatAC = fort = reflex = will = speed = reach = initiative = BAB = CMB = CMD = 0;
+            //Initialize Non-Knowledge Skills
+            sklAcro = sklAppr = sklBluf = sklClim = 0;
+            sklCraf = sklDipl = sklDisa = sklDisg = 0;
+            sklEsca = sklFly = sklHand = sklHeal = 0;
+            sklInti = sklLing = sklPerc = sklPerf = 0;
+            sklProf = sklRide = sklSens = sklSlei = 0;
+            sklSpel = sklStel = sklSurv = sklSwim = 0;
+            sklUseD = 0;
+            //Initialize Knowledge Skills
+            knwArca = knwDung = knwEngi = knwGeog = 0;
+            knwHist = knwLoca = knwNatu = knwNobi = 0;
+            knwPlan = knwReli = 0;
+            //Initialize Non-Knowledge Skill Trained Status
+            acroTrn = apprTrn = blufTrn = climTrn = false;
+            crafTrn = diplTrn = disaTrn = disgTrn = false;
+            escaTrn = flyTrn = handTrn = healTrn = false;
+            intiTrn = lingTrn = percTrn = perfTrn = false;
+            profTrn = rideTrn = sensTrn = sleiTrn = false;
+            spelTrn = steaTrn = survTrn = swimTrn = false;
+            useMTrn = false;
+            //Initialize Knowledge Skill Trained Status
+            arcaTrn = dungTrn = engiTrn = geogTrn = false;
+            histTrn = locaTrn = natuTrn = nobiTrn = false;
+            planTrn = reliTrn = false;
+        }
+
+        public statBlockPF(int id, string newName, string pName, string race)
+        {
+            blockID = id;
+            name = newName;
+            playerName = pName;
+            raceClass = race;
+            //Initialize Non-Knowledge Skills
+            STR = CON = DEX = INT = WIS = CHA = 10;
+            AC = touchAC = flatAC = fort = reflex = will = speed = reach = initiative = BAB = CMB = CMD = 0;
+            sklAcro = sklAppr = sklBluf = sklClim = 0;
+            sklCraf = sklDipl = sklDisa = sklDisg = 0;
+            sklEsca = sklFly = sklHand = sklHeal = 0;
+            sklInti = sklLing = sklPerc = sklPerf = 0;
+            sklProf = sklRide = sklSens = sklSlei = 0;
+            sklSpel = sklStel = sklSurv = sklSwim = 0;
+            sklUseD = 0;
+            //Initialize Knowledge Skills
+            knwArca = knwDung = knwEngi = knwGeog = 0;
+            knwHist = knwLoca = knwNatu = knwNobi = 0;
+            knwPlan = knwReli = 0;
+            //Initialize Non-Knowledge Skill Trained Status
+            acroTrn = apprTrn = blufTrn = climTrn = false;
+            crafTrn = diplTrn = disaTrn = disgTrn = false;
+            escaTrn = flyTrn = handTrn = healTrn = false;
+            intiTrn = lingTrn = percTrn = perfTrn = false;
+            profTrn = rideTrn = sensTrn = sleiTrn = false;
+            spelTrn = steaTrn = survTrn = swimTrn = false;
+            useMTrn = false;
+            //Initialize Knowledge Skill Trained Status
+            arcaTrn = dungTrn = engiTrn = geogTrn = false;
+            histTrn = locaTrn = natuTrn = nobiTrn = false;
+            planTrn = reliTrn = false;
+        }
+    }
+
+    class charStatList
+    {
+        LinkedList<statBlockPF> charList = new LinkedList<statBlockPF>();
+
+        private statBlockPF error = new statBlockPF(-1, "<!>ERROR", "<!>ERROR", "<!>ERROR");
+
+        public void clearList()
+        {
+            charList.Clear();
+        }
+
+        //Get first payload in list
+        public statBlockPF getFirst()
+        {
+
+            if (charList.Count != 0)
+            {
+                return charList.First.Value;
+            }
+            else
+            {
+                return error;
+            }
+
+        }
+
+        //Get payload stored next after specified blockID
+        public statBlockPF getNext(int targetID)
+        {
+            LinkedListNode<statBlockPF> current = charList.First;
+
+            while (current != null)
+            {
+
+                if (current.Value.blockID == targetID)
+                {
+                    if (current.Next != null)
+                    {
+                        return current.Next.Value;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                current = current.Next;
+            }
+            return error;
+        }
+
+        //Get payload stored next before specified blockID
+        public statBlockPF getPrev(int targetID)
+        {
+            LinkedListNode<statBlockPF> current = charList.First;
+
+            while (current != null)
+            {
+
+                if (current.Value.blockID == targetID)
+                {
+                    if (current.Previous != null)
+                    {
+                        return current.Previous.Value;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                current = current.Next;
+            }
+            return error;
+        }
+
+        //Get last payload in list
+        public statBlockPF getLast()
+        {
+
+            if (charList.Count != 0)
+            {
+                return charList.Last.Value;
+            }
+            else
+            {
+                return error;
+            }
+
+        }
+
+        //Add specified payload to list or update entry
+        public Boolean addEntry(statBlockPF newBlock)
+        {
+
+            if (charList.Count == 0)
+            {
+                charList.AddFirst(newBlock);
+                return true;
+            }
+
+            LinkedListNode<statBlockPF> current = charList.First;
+
+            while (current != null)
+            {
+
+                if (current.Value.blockID == newBlock.blockID)
+                {               //If updating an entry
+                    current.Value = newBlock;
+                    return true;
+                }
+                else if (current.Value.blockID > newBlock.blockID)
+                {         //Inserting within list
+                    charList.AddBefore(current, newBlock);
+                    return true;
+                }
+                else if (current == charList.Last)
+                {                         //Inserting at end of list
+                    charList.AddAfter(current, newBlock);
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        //Remove payload with specified ID from list
+        public Boolean removeEntry(int targetID)
+        {
+            LinkedListNode<statBlockPF> current = charList.First;
+
+            while (current != null)
+            {
+
+                if (current.Value.blockID == targetID)
+                {
+                    charList.Remove(current);
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        //Retrieve payload with specified 
+        public statBlockPF retrieveEntry(int targetID)
+        {
+            LinkedListNode<statBlockPF> current = charList.First;
+
+            while (current != null)
+            {
+
+                if (current.Value.blockID == targetID)
+                {
+                    return current.Value;
+                }
+
+                current = current.Next;
+            }
+
+            return error;
+        }
+
+        public int getLength()
+        {
+            return charList.Count();
+        }
+    }
+}
